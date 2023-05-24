e8ec0c6c430269cf7720e3df7921bcdd12eccecd
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-05-24 18:55:21 +0530

    Add files via upload

---
+++ src/main/java/com/iiht/evaluation/automation/UITest.java
@@ -0,0 +1,69 @@
+package com.iiht.evaluation.automation;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
+//import org.openqa.selenium.WebElement;
+//import java.util.List;
+import org.openqa.selenium.StaleElementReferenceException;
+
+public class UITest {
+	
+	public static void preset() {
+
+	}
+
+
+	public static boolean mouseOverPersonalFinance(WebDriver driver, String baseUrl)  {
+		return false;
+
+	}
+
+	public static boolean clickTools(WebDriver driver, Actions actions)  {
+		return false;
+	}
+	
+	public static boolean clickOnHomeLoanEMICalculator(WebDriver driver) {
+		return false;
+	}
+
+	public static boolean enterValueLoanAmount(WebDriver driver) 	{
+		return false;
+	}
+	public static boolean enterValueLoanPeriod(WebDriver driver) {
+		return false;
+	}
+	public static boolean selectEMIStartsFrom(WebDriver driver) {
+		return false;
+	}
+	public static boolean enterInterestRate(WebDriver driver) {
+		return false;
+	}
+	
+	public static boolean enterValueUpfrontCharges(WebDriver driver) {
+		return false;
+	}
+	public static boolean clickSubmit(WebDriver driver){
+		return false;
+	}
+	
+	
+	
+	
+
+	
+	public static void closeAll() {
+	}
+
+
+	
+}
